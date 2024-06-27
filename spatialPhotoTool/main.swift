95a949526b49847a4cb024886fc1e5d2bc5ee5f4
Scott Jann <sjann@knight-rider.org>
2024-06-27 00:10:45 -0500

    update image metadata
    - write baseline/pupil distance to image
    - add missing properties based on Apple's example code at https://developer.apple.com/documentation/imageio/writing-spatial-photos
    - adjust timestamps for QooCam from UTC to local time

--- spatialPhotoTool/main.swift
+++ spatialPhotoTool/main.swift
@@ -16,6 +16,10 @@ struct SpatialPhotoTool: ParsableCommand {
 	@Argument var files: [String] = []
 	@Option(name: .customLong("hfov"), help: "Horizontal field-of-view (in degrees).")
 	var hFOV: Double?
+	@Option(name: .customLong("disparityAdjustment"), help: "Disparity adjustment, -1.0 to 1.0.")
+	var disparityAdjustment: Double?
+	@Option(name: .customLong("baseline"), help: "Baseline (IPD) of lenses in mm.")
+	var baseline: Double?
 
 	static let configuration = CommandConfiguration(commandName: "spatialPhotoTool")
 
@@ -38,6 +42,17 @@ struct SpatialPhotoTool: ParsableCommand {
 				print("Unknown file specified: \(url.lastPathComponent)")
 			}
 		}
+	}
+
+	/// the QooCam writes the date in UTC, so adjust it to the local time
+	private func offsetDate(_ dateString: String) -> String {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy:MM:dd HH:mm:ss"
+
+		guard let date = dateFormatter.date(from: dateString) else { return dateString }
+
+		let offset = TimeInterval(TimeZone.current.secondsFromGMT(for: date))
+		return dateFormatter.string(from: (date + offset))
 	}
 
 	private func convertMPO(_ url: URL) {
@@ -100,7 +115,7 @@ struct SpatialPhotoTool: ParsableCommand {
 			return
 		}
 
-		createSpatialImage(URL(fileURLWithPath: url.deletingPathExtension().path(percentEncoded: false) + ".heic"), left: images[0], right: images[1], metadata: properties, hFOV: hFOV ?? 48.0)
+		createSpatialImage(URL(fileURLWithPath: url.deletingPathExtension().path(percentEncoded: false) + ".heic"), left: images[0], right: images[1], metadata: properties, hFOV: hFOV ?? 48.0, baseline: baseline ?? 75.0)
 	}
 
 	private func convertSBS(_ url: URL) {
@@ -141,21 +156,71 @@ struct SpatialPhotoTool: ParsableCommand {
 					print("Adding QooCam EGO Camera info...")
 					tiffProperties.updateValue("Kandao", forKey: kCGImagePropertyTIFFMake)
 					tiffProperties.updateValue("QooCam EGO", forKey: kCGImagePropertyTIFFModel)
+
+					if let originalDate = tiffProperties[kCGImagePropertyTIFFDateTime] as? String {
+						tiffProperties.updateValue(offsetDate(originalDate), forKey: kCGImagePropertyTIFFDateTime)
+					}
+
 					properties.updateValue(tiffProperties as CFDictionary, forKey: kCGImagePropertyTIFFDictionary)
+				}
+
+				if var exifProperties = properties[kCGImagePropertyExifDictionary] as? Dictionary<CFString, Any> {
+					if let originalDate = exifProperties[kCGImagePropertyExifDateTimeOriginal] as? String {
+						exifProperties.updateValue(offsetDate(originalDate), forKey: kCGImagePropertyExifDateTimeOriginal)
+					}
+					if let originalDate = exifProperties[kCGImagePropertyExifDateTimeDigitized] as? String {
+						exifProperties.updateValue(offsetDate(originalDate), forKey: kCGImagePropertyExifDateTimeDigitized)
+					}
+					properties.updateValue(exifProperties as CFDictionary, forKey: kCGImagePropertyExifDictionary)
 				}
 			}
 		}
 
-		createSpatialImage(URL(fileURLWithPath: url.deletingPathExtension().path(percentEncoded: false) + ".heic"), left: left, right: right, metadata: properties as CFDictionary, hFOV: hFOV ?? 66.0)
+		createSpatialImage(URL(fileURLWithPath: url.deletingPathExtension().path(percentEncoded: false) + ".heic"), left: left, right: right, metadata: properties as CFDictionary, hFOV: hFOV ?? 66.0, baseline: baseline ?? 65.0)
 	}
 
-	func createSpatialImage(_ url: URL, left: CGImage, right: CGImage, metadata: CFDictionary, hFOV: Double) {
+	func propertiesDictionary(isLeft: Bool, disparityAdjustment: Double?, position: [Double], intrinsics: [CGFloat], metadata: CFDictionary) -> [CFString: Any] {
+		let rotationMatrix: [CGFloat] = [
+			1, 0, 0,
+			0, 1, 0,
+			0, 0, 1
+		]
+
+		let positionMatrix: [CGFloat] = [ 0, 0, 0 ]
+		let encodedDisparityAdjustment = Int((disparityAdjustment ?? 0) * 1e4)
+
+		var properties = (metadata as? Dictionary<String, Any>) ?? [String: Any]()
+
+		properties[kCGImagePropertyGroups as String] = [
+			kCGImagePropertyGroupIndex: 0,
+			kCGImagePropertyGroupType: kCGImagePropertyGroupTypeStereoPair,
+			(isLeft ? kCGImagePropertyGroupImageIsLeftImage : kCGImagePropertyGroupImageIsRightImage): true,
+			kCGImagePropertyGroupImageDisparityAdjustment: encodedDisparityAdjustment,
+		]
+		properties[kCGImagePropertyHEIFDictionary as String] = [
+			kIIOMetadata_CameraModelKey: [
+				kIIOCameraModel_Intrinsics: intrinsics as CFArray,
+				kIIOCameraModel_ModelType: kIIOCameraModelType_SimplifiedPinhole,
+			],
+			kIIOMetadata_CameraExtrinsicsKey: [
+				kIIOCameraExtrinsics_CoordinateSystemID: 0 as CGFloat,
+				kIIOCameraExtrinsics_Position: positionMatrix as CFArray,
+				kIIOCameraExtrinsics_Rotation: rotationMatrix as CFArray,
+			]
+		]
+		properties[kCGImagePropertyHasAlpha as String] = false
+
+		return properties as [CFString: Any]
+	}
+
+	func createSpatialImage(_ url: URL, left: CGImage, right: CGImage, metadata: CFDictionary, hFOV: Double, baseline: Double) {
 		guard left.width == right.width && left.height == right.height else {
 			print("Image sizes are mismatched: \(url.lastPathComponent)")
 			return
 		}
 		print("Saving images to \(url.path(percentEncoded: false))")
 		print("Using hFOV=\(hFOV)")
+		print("Using baseline=\(baseline)")
 		print("image size is \(left.width)x\(left.height) + \(right.width)x\(right.height)")
 
 		let destination = CGImageDestinationCreateWithURL(url as CFURL, UTType.heic.identifier as CFString, 2, nil)!
@@ -171,36 +236,9 @@ struct SpatialPhotoTool: ParsableCommand {
 			0, 0, 1
 		]
 
-		let rotationMatrix: [CGFloat] = [
-			1, 0, 0,
-			0, 1, 0,
-			0, 0, 1
-		]
-
-		let positionMatrix: [CGFloat] = [ 0, 0, 0 ]
-
-		guard var properties = metadata as? Dictionary<String, Any> else {
-			return
-		}
-		properties[kCGImagePropertyGroups as String] = [
-			kCGImagePropertyGroupIndex: 0,
-			kCGImagePropertyGroupType: kCGImagePropertyGroupTypeStereoPair,
-			kCGImagePropertyGroupImageIndexLeft: 0,
-			kCGImagePropertyGroupImageIndexRight: 1,
-		]
-		properties[kCGImagePropertyHEIFDictionary as String] = [
-			kIIOMetadata_CameraModelKey: [
-				kIIOCameraModel_Intrinsics: cameraIntrinsics as CFArray,
-			],
-			kIIOMetadata_CameraExtrinsicsKey: [
-				kIIOCameraExtrinsics_CoordinateSystemID: 0 as CGFloat,
-				kIIOCameraExtrinsics_Position: positionMatrix as CFArray,
-				kIIOCameraExtrinsics_Rotation: rotationMatrix as CFArray,
-			]
-		]
-
-		CGImageDestinationAddImage(destination, left, properties as CFDictionary)
-		CGImageDestinationAddImage(destination, right, properties as CFDictionary)
+		CGImageDestinationAddImage(destination, left, propertiesDictionary(isLeft: true, disparityAdjustment: disparityAdjustment, position: [0, 0, 0], intrinsics: cameraIntrinsics, metadata: metadata) as CFDictionary)
+		let baselineInMeters = baseline / 1000.0
+		CGImageDestinationAddImage(destination, right, propertiesDictionary(isLeft: false, disparityAdjustment: disparityAdjustment, position: [baselineInMeters, 0, 0], intrinsics: cameraIntrinsics, metadata: metadata) as CFDictionary)
 		CGImageDestinationFinalize(destination)
 	}
 }
